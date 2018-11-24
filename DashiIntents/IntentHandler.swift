49b50d01356d3007d9a33c085e0d7491ed010b30
Kaname Noto <noto@irimasu.com>
2018-11-24 09:39:47 +0900

    Add create note intent support.

--- DashiIntents/IntentHandler.swift
+++ DashiIntents/IntentHandler.swift
@@ -7,7 +7,7 @@
 //
 
 import Intents
-import SlackKit
+import DashiKit
 
 // https://medium.com/@pietropizzi/a-beginners-guide-to-developing-custom-intent-siri-shortcuts-for-ios-12-a3627b7011af
 
@@ -17,13 +17,35 @@ class IntentHandler: INExtension {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
         
-        guard intent is DashiSendIntent else {
-            fatalError("Unhandled intent type: \(intent)")
+        if intent is DashiSendIntent {
+            return DashiSendIntentHandler()
         }
         
-        return DashiSendIntentHandler()
+        return self
     }
     
 }
 
+extension IntentHandler : INCreateNoteIntentHandling
+{
+    func handle(intent: INCreateNoteIntent, completion: @escaping (INCreateNoteIntentResponse) -> Void) {
+        
+        if let textContent = intent.content as? INTextNoteContent {
+            PostUseCase.shared.post(textContent.text ?? "" ) { (error) in
+                if let error = error {
+                    let response = INCreateNoteIntentResponse(code: .failure, userActivity: nil)
+                    completion(response)
+                } else {
+                    let response = INCreateNoteIntentResponse(code: .success , userActivity: nil)
+                    response.createdNote = INNote(title: INSpeakableString(spokenPhrase: "general"), contents: [intent.content!], groupName: nil, createdDateComponents: nil, modifiedDateComponents: nil, identifier: nil)
+                    
+                    completion(response)
+                }
+            }
+        }else{
+            let response = INCreateNoteIntentResponse(code: .failure, userActivity: nil)
+            completion(response)
+        }
+    }
+}
 
